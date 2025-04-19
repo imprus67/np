e5b68c6f509474dc38b4b6ca73ce7a52523fa8ff
konst <my@email.com>
2025-04-19 22:55:04 +0300

    Second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito, Pacifico } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/shared/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const nunito = Nunito({
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const dancing = Pacifico({
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={nunito.className}>
+        <main className="min-h-screen">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -8,13 +8,14 @@ interface Props {
 }
 
 const cats = [
-  "Пиццы",
-  "Комбо",
-  "Закуски",
-  "Коктейли",
-  "Кофе",
-  "Напитки",
-  "Десерты",
+  { id: 1, name: "Пиццы" },
+  { id: 2, name: "Комбо" },
+  { id: 3, name: "Закуски" },
+  { id: 4, name: "Коктейли" },
+  { id: 5, name: "Кофе" },
+  { id: 6, name: "Напитки" },
+  { id: 7, name: "Десерты" },
+  { id: 8, name: "Десерты" },
 ];
 
 export const Categories: React.FC<Props> = ({ className }) => {
@@ -23,16 +24,17 @@ export const Categories: React.FC<Props> = ({ className }) => {
     <div
       className={cn("inline-flex gap-1 bg-gray-200 p-1 rounded-2xl", className)}
     >
-      {cats.map((cat, index) => (
+      {cats.map(({ name, id }, index) => (
         <a
           className={cn(
             "flex items center font-bold h-11 rounded-2xl px-5",
-            categoryActiveId === index &&
+            categoryActiveId === id &&
               "bg-white text-primary shadow-[0px_0px_19px_-12px_rgba(0,_0,_0,_0.8)]",
           )}
+          href={`/#${name}`}
           key={index}
         >
-          <button className={cn("cursor-pointer", className)}>{cat}</button>
+          <button className={cn("cursor-pointer", className)}>{name}</button>
         </a>
       ))}
     </div>
