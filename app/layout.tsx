a5a08a30a6395dbd24d2b27572ca46d0f49c257d
konst <my@email.com>
2025-04-28 08:32:47 +0300

    Add layouts

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Nunito, Pacifico } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const dancing = Pacifico({
+  weight: "400",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link data-rh="true" rel="icon" href="/logo.png" />
+      </head>
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
