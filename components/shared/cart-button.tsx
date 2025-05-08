6cad2ee1259e087282167bebf5bb31d6ad1dfd55
konst <my@email.com>
2025-05-08 11:18:35 +0300

    Cart start commit

--- components/shared/cart-button.tsx
+++ components/shared/cart-button.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import { Button } from "../ui";
 import { ArrowRight, ShoppingCart } from "lucide-react";
+import { CartDrawer } from "./cart-drawer";
 
 interface Props {
   className?: string;
@@ -11,21 +12,23 @@ interface Props {
 
 export const CartButton: React.FC<Props> = ({ className }) => {
   return (
-    <Button
-      className={cn(
-        "group relative",
-        /* { "w-[105px]": loading }, */ className,
-      )}
-    >
-      <span className="h-full w-[1px] bg-white/30 mx-3" />
-      <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-        /* <ShoppingCart size={16} className="relative" strokeWidth={2} />
-        */
-      </div>
-      <ArrowRight
-        size={20}
-        className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
-      />
-    </Button>
+    <CartDrawer>
+      <Button
+        className={cn(
+          "group relative w-[105px] cursor-pointer",
+          /* { "w-[105px]": loading }, */ className,
+        )}
+      >
+        <b>0 ₽</b>
+        <span className="h-full w-[1px] bg-white/30 mx-3" />
+        <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+          <ShoppingCart size={16} className="relative" strokeWidth={2} />
+        </div>
+        <ArrowRight
+          size={20}
+          className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+        />
+      </Button>
+    </CartDrawer>
   );
 };
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -16,4 +16,6 @@ export { ChooseProductForm } from "./choose-product-form";
 export { ChoosePizzaForm } from "./choose-pizza-form";
 export { IngredientItem } from "./ingredient-item";
 export { CartButton } from "./cart-button";
+export { CartDrawer } from "./cart-drawer";
+export { CartDrawerItem } from "./cart-drawer-item";
 export * from "./modals";
